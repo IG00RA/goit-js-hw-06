81efe42d28980a4489f9fa1f5adc0dc05f4e80f2
Igor Gromadskiy <gromadskiyigor@gmail.com>
2023-02-16 00:23:07 +0200

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -3,6 +3,7 @@ const maxLength = inputObj.attributes[2].value;
 inputObj.addEventListener("blur", submitFunc);
 function submitFunc() {
   if (inputObj.value.length > maxLength) {
+    inputObj.classList.remove("valid");
     return inputObj.classList.add("invalid");
   }
   inputObj.classList.remove("invalid");
