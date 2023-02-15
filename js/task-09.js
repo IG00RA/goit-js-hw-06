1b9fd29526cbf67fcfdbf928784dea0a3294b821
Igor Gromadskiy <gromadskiyigor@gmail.com>
2023-02-15 22:43:23 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,11 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const spanObj = document.querySelector(".color");
+const buttonObj = document.querySelector(".change-color");
+const bodyObj = document.querySelector("body");
+function changeColorFunc() {
+  bodyObj.style.backgroundColor = getRandomHexColor();
+  spanObj.textContent = getRandomHexColor();
+}
+buttonObj.addEventListener("click", changeColorFunc);
