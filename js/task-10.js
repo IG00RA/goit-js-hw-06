cfe8c849692e1099e3b82c43dfec785e54e770b5
Igor Gromadskiy <gromadskiyigor@gmail.com>
2023-02-16 00:00:42 +0200

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,36 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const inputObj = document.querySelector("input[type='number']");
+const createObj = document.querySelector("button[data-create]");
+const destroyObj = document.querySelector("button[data-destroy]");
+const divObj = document.querySelector("#boxes");
+let amount = 1;
+function inputFunction() {
+  amount = inputObj.value;
+}
+function createBoxes(amount) {
+  destroyFunction();
+  const boxes = [];
+  let minSize = 30;
+  for (let i = 0; i < amount; i += 1) {
+    minSize += 10;
+    const divEl = document.createElement("div");
+    divEl.style.width = `${minSize}px`;
+    divEl.style.height = `${minSize}px`;
+    divEl.style.backgroundColor = getRandomHexColor();
+    boxes.push(divEl);
+    divObj.append(...boxes);
+  }
+}
+createObj.addEventListener("click", createFunction);
+inputObj.addEventListener("input", inputFunction);
+destroyObj.addEventListener("click", destroyFunction);
+
+function createFunction() {
+  createBoxes(amount);
+}
+function destroyFunction() {
+  divObj.innerHTML = "";
+}
