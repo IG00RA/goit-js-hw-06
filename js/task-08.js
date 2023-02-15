768d9c10752e6a6e43fa2c318cfc78c952607339
Igor Gromadskiy <gromadskiyigor@gmail.com>
2023-02-15 22:21:41 +0200

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,22 @@
+const formObj = document.querySelector(".login-form");
+const emailObj = document.querySelector('input[name="email"]');
+const passwObj = document.querySelector('input[name="password"]');
+const submitObj = document.querySelector('button[type="submit"]');
 
+formObj.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+  if (emailObj.value === "" || passwObj.value === "") {
+    return alert("Please enter a valid value");
+  }
+  const inputData = {
+    email: email.value,
+    password: password.value,
+  };
+  console.log(inputData);
+  event.currentTarget.reset();
+}
