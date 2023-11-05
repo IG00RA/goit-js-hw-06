a81c82e09b5c20d44475d2b405d244b4c8f8168f
Igor Gromadskiy <gromadskiyigor@gmail.com>
2023-11-05 15:19:27 +0200

    10 task in TS

--- js/task-10.js
+++ js/task-10.js
@@ -1,36 +1,32 @@
-function getRandomHexColor() {
-    return `#${Math.floor(Math.random() * 16777215)
-        .toString(16)
-        .padStart(6, 0)}`;
-}
 const inputElem = document.querySelector("input[type='number']");
 const createElem = document.querySelector("button[data-create]");
 const destroyElem = document.querySelector("button[data-destroy]");
 const divElem = document.querySelector("#boxes");
-let amount = 0;
-function inputFunction() {
-    amount = inputElem.value;
-}
-function createBoxes(amount) {
-    destroyFunction();
-    const boxes = [];
-    let minSize = 30;
-    for (let i = 0; i < amount; i += 1) {
-        minSize += 10;
-        const divEl = document.createElement("div");
-        divEl.style.width = `${minSize}px`;
-        divEl.style.height = `${minSize}px`;
-        divEl.style.backgroundColor = getRandomHexColor();
-        boxes.push(divEl);
+createElem === null || createElem === void 0 ? void 0 : createElem.addEventListener("click", createBoxes);
+inputElem === null || inputElem === void 0 ? void 0 : inputElem.addEventListener("input", createBoxes);
+destroyElem === null || destroyElem === void 0 ? void 0 : destroyElem.addEventListener("click", destroyBoxes);
+function createBoxes() {
+    const amount = Number(inputElem === null || inputElem === void 0 ? void 0 : inputElem.value);
+    destroyBoxes();
+    if (divElem) {
+        const boxes = [];
+        let minSize = 30;
+        for (let i = 0; i < amount; i += 1) {
+            minSize += 10;
+            const divEl = document.createElement("div");
+            divEl.style.width = `${minSize}px`;
+            divEl.style.height = `${minSize}px`;
+            divEl.style.backgroundColor = getRandomHexColor();
+            boxes.push(divEl);
+        }
         divElem.append(...boxes);
     }
 }
-createElem.addEventListener("click", createFunction);
-inputElem.addEventListener("input", inputFunction);
-destroyElem.addEventListener("click", destroyFunction);
-function createFunction() {
-    createBoxes(amount);
+function destroyBoxes() {
+    if (divElem) {
+        divElem.innerHTML = "";
+    }
 }
-function destroyFunction() {
-    divElem.innerHTML = "";
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
--- src/task-10.ts
+++ src/task-10.ts
@@ -1,38 +1,45 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
-}
+const inputElem = document.querySelector(
+  "input[type='number']"
+) as HTMLInputElement | null;
+const createElem = document.querySelector(
+  "button[data-create]"
+) as HTMLButtonElement | null;
+const destroyElem = document.querySelector(
+  "button[data-destroy]"
+) as HTMLButtonElement | null;
+const divElem = document.querySelector("#boxes") as HTMLDivElement | null;
 
-const inputElem = document.querySelector("input[type='number']");
-const createElem = document.querySelector("button[data-create]");
-const destroyElem = document.querySelector("button[data-destroy]");
-const divElem = document.querySelector("#boxes");
-let amount = 0;
-function inputFunction() {
-  amount = inputElem.value;
-}
-function createBoxes(amount) {
-  destroyFunction();
-  const boxes = [];
-  let minSize = 30;
-  for (let i = 0; i < amount; i += 1) {
-    minSize += 10;
-    const divEl = document.createElement("div");
-    divEl.style.width = `${minSize}px`;
-    divEl.style.height = `${minSize}px`;
-    divEl.style.backgroundColor = getRandomHexColor();
-    boxes.push(divEl);
+createElem?.addEventListener("click", createBoxes);
+inputElem?.addEventListener("input", createBoxes);
+destroyElem?.addEventListener("click", destroyBoxes);
+
+function createBoxes(): void {
+  const amount = Number(inputElem?.value);
+  destroyBoxes();
+
+  if (divElem) {
+    const boxes: HTMLDivElement[] = [];
+    let minSize = 30;
+
+    for (let i = 0; i < amount; i += 1) {
+      minSize += 10;
+      const divEl = document.createElement("div");
+      divEl.style.width = `${minSize}px`;
+      divEl.style.height = `${minSize}px`;
+      divEl.style.backgroundColor = getRandomHexColor();
+      boxes.push(divEl);
+    }
+
     divElem.append(...boxes);
   }
 }
-createElem.addEventListener("click", createFunction);
-inputElem.addEventListener("input", inputFunction);
-destroyElem.addEventListener("click", destroyFunction);
 
-function createFunction() {
-  createBoxes(amount);
+function destroyBoxes() {
+  if (divElem) {
+    divElem.innerHTML = "";
+  }
 }
-function destroyFunction() {
-  divElem.innerHTML = "";
+
+function getRandomHexColor(): string {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
